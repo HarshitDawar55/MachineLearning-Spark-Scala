065929dc006e5bf94b13e7f29d9a17317f35282b
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-09-16 17:51:09 +0530

    Started the Project

--- E-Commerce-Customers-Regression-Model/E-Commerce-Customers-Regression.scala
+++ E-Commerce-Customers-Regression-Model/E-Commerce-Customers-Regression.scala
@@ -6,6 +6,17 @@ import org.apache.spark.ml.regression
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.sql.SparkSession
 
 Logger.getLogger("org").setLevel(Level.ERROR)
 
+// Creating a Spark Session
+val spark = SparkSession.builder().config("spark.master", "local").getOrCreate()
+
+// Loaded the Data
+var data = spark.read.option("header", "true").option("inferSchema",
+  "true").format("csv").load(
+  "file:////Users/harshitdawar/Github/Classic/MachineLearningWithSpark&Scala/src/main/scala/Datasets/Ecommerce-Customers.csv")
+
+// Printing the Schema of the Project
+println(data.printSchema())
