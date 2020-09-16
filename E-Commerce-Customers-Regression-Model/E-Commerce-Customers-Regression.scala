8b013166518e2ba04757017f7c063a019549edf1
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-09-16 17:47:41 +0530

    Imported the required Libraries

--- E-Commerce-Customers-Regression-Model/E-Commerce-Customers-Regression.scala
+++ E-Commerce-Customers-Regression-Model/E-Commerce-Customers-Regression.scala
@@ -2,3 +2,10 @@
 //// Importing the required Libraries ///////////
 /////////////////////////////////////////
 
+import org.apache.spark.ml.regression
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.regression.LinearRegression
+
+Logger.getLogger("org").setLevel(Level.ERROR)
+
